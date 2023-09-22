6c1d4471d4327a157da28352634ab8e2bf1c0beb
mannamsarath <mannamsarath58@gmail.com>
2023-09-22 09:39:41 +0530

    added servers script

--- create_ec2.sh
+++ create_ec2.sh
@@ -2,9 +2,20 @@
 
 # creating an Array
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+Image_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0a92292ce4af7fec5
 
+# For mysql & mongodb instance_type should be t3.medium, for all others t2.micro. So this is the condition we should check. 
 # to loop through the array is, now we will get all these names into this for loop
 for i  in "${NAMES[@]}" 
 do
-   echo "NAME: $i" 
+   if [ [ $i == "mongodb" || $i == "mysql" ] ]
+   then 
+        INSTANCE_TYPE="t3.medium"
+   else
+        INSTANCE_TYPE="t2.micro"
+   fi        
+   echo "creating $i instance"
+   aws ec2 run-instances --image-id $Image_ID --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=demo-$i}]"
 done
